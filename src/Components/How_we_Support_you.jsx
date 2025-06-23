0967878c3be376a43ef0fa66918cd4ba3ecf226d
SudeepNPatil <sudeepbsc02@gmail.com>
2025-06-23 13:01:48 +0530

    updated

--- src/Components/How_we_Support_you.jsx
+++ src/Components/How_we_Support_you.jsx
@@ -17,7 +17,7 @@ export default function How_we_Support_you() {
                     </div>
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold">Ready2go Project</h1>
-                        <p className="text-gray-700 mt-1 w-[65vw] mb-1">List of pre-built, well-documented academic projects. Instant order and quick delivery.</p>
+                        <p className="text-gray-700 mt-1 mb-1">List of pre-built, well-documented academic projects. Instant order and quick delivery.</p>
                     </div>
                 </div>
             </ScrollReveal>
@@ -31,7 +31,7 @@ export default function How_we_Support_you() {
 
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold">Custom Build Service</h1>
-                        <p className="text-gray-700 mt-1 w-[65vw]">Students submit their idea and requirements, our team builds and delivers the project.</p>
+                        <p className="text-gray-700 mt-1">Students submit their idea and requirements, our team builds and delivers the project.</p>
                     </div>
                 </div>
             </ScrollReveal>
@@ -45,7 +45,7 @@ export default function How_we_Support_you() {
 
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold">Project Navigator</h1>
-                        <p className="text-gray-700 mt-1 w-[65vw]">Roadmap, tech stack suggestions, deployment tips, and full mentorship for self-builders.</p>
+                        <p className="text-gray-700 mt-1 ">Roadmap, tech stack suggestions, deployment tips, and full mentorship for self-builders.</p>
                     </div>
                 </div>
             </ScrollReveal>
@@ -59,7 +59,7 @@ export default function How_we_Support_you() {
 
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold"> Debug & Rescue</h1>
-                        <p className="text-gray-700 mt-1 w-[65vw]">One-on-one issue resolution, bug fixing, and expert suggestions to continue the project.</p>
+                        <p className="text-gray-700 mt-1">One-on-one issue resolution, bug fixing, and expert suggestions to continue the project.</p>
                     </div>
                 </div>
             </ScrollReveal>
