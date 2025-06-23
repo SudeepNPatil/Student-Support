d9b74f085c62789a970459cac50cfc0f46c8e3f3
SudeepNPatil <sudeepbsc02@gmail.com>
2025-06-23 13:10:19 +0530

    updated

--- src/Components/How_we_Support_you.jsx
+++ src/Components/How_we_Support_you.jsx
@@ -10,56 +10,56 @@ export default function How_we_Support_you() {
             </ScrollReveal>
 
             <ScrollReveal>
-                <div className="flex gap-5  transition-all duration-500">
-                    <div className="flex flex-col gap-2  flex-wrap items-center">
+                <div className="flex gap-5 mt-2  transition-all duration-500">
+                    <div className="flex flex-col gap-4  flex-wrap items-center">
                         <button className="border-2 border-black w-7 h-7 rounded-3xl">1</button>
                         <div className="w-2 flex-auto bg-blue-200 rounded-sm"></div>
                     </div>
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold">Ready2go Project</h1>
-                        <p className="text-gray-700 mt-1 mb-1">List of pre-built, well-documented academic projects. Instant order and quick delivery.</p>
+                        <p className="text-gray-700 mt-4 mb-1">List of pre-built, well-documented academic projects. Instant order and quick delivery.</p>
                     </div>
                 </div>
             </ScrollReveal>
 
             <ScrollReveal>
-                <div className="flex gap-5 mt-1 transition-all duration-500">
-                    <div className="flex flex-col gap-2 flex-wrap items-center">
+                <div className="flex gap-5 mt-4 transition-all duration-500">
+                    <div className="flex flex-col gap-4 flex-wrap items-center">
                         <button className="border-2 border-black w-7 h-7 rounded-3xl">2</button>
                         <div className="w-2 flex-auto bg-blue-200 rounded-sm"></div>
                     </div>
 
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold">Custom Build Service</h1>
-                        <p className="text-gray-700 mt-1">Students submit their idea and requirements, our team builds and delivers the project.</p>
+                        <p className="text-gray-700 mt-4">Students submit their idea and requirements, our team builds and delivers the project.</p>
                     </div>
                 </div>
             </ScrollReveal>
 
             <ScrollReveal>
-                <div className="flex gap-5 mt-1  transition-all duration-500">
-                    <div className="flex flex-col gap-2 flex-wrap items-center">
+                <div className="flex gap-5 mt-4  transition-all duration-500">
+                    <div className="flex flex-col gap-4 flex-wrap items-center">
                         <button className="border-2 border-black w-7 h-7 rounded-3xl">3</button>
                         <div className="flex-auto w-2 bg-blue-200 rounded-sm"></div>
                     </div>
 
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold">Project Navigator</h1>
-                        <p className="text-gray-700 mt-1 ">Roadmap, tech stack suggestions, deployment tips, and full mentorship for self-builders.</p>
+                        <p className="text-gray-700 mt-4">Roadmap, tech stack suggestions, deployment tips, and full mentorship for self-builders.</p>
                     </div>
                 </div>
             </ScrollReveal>
 
             <ScrollReveal>
-                <div className="flex gap-5 mt-1 transition-all duration-500">
-                    <div className="flex flex-col gap-2 flex-wrap items-center">
+                <div className="flex gap-5 mt-4 transition-all duration-500">
+                    <div className="flex flex-col gap-4 flex-wrap items-center">
                         <button className="border-2 border-black w-7 h-7 rounded-3xl">4</button>
                         <div className="flex-auto w-2 bg-blue-200 rounded-sm"></div>
                     </div>
 
                     <div className="flex flex-col">
                         <h1 className="text-[17px] font-semibold"> Debug & Rescue</h1>
-                        <p className="text-gray-700 mt-1">One-on-one issue resolution, bug fixing, and expert suggestions to continue the project.</p>
+                        <p className="text-gray-700 mt-4">One-on-one issue resolution, bug fixing, and expert suggestions to continue the project.</p>
                     </div>
                 </div>
             </ScrollReveal>
