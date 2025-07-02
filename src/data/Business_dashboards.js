bb28344e77a0ca32f199e983cc784227c89768e6
SudeepNPatil <sudeepbsc02@gmail.com>
2025-07-02 23:12:48 +0530

    data collected

---
+++ src/data/Business_dashboards.js
@@ -0,0 +1,50 @@
+const Business_Dashboards = [
+    {
+        "image_url": "",
+        "title": "Sales Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["React js", "Recharts/Chart.js", "Tailwindcss"]
+    },
+    {
+        "image_url": "",
+        "title": "Marketing Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Connect Google Analytics API (optional)"]
+    },
+    {
+        "image_url": "",
+        "title": "HR Dashboard(Employee Analytics)",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Progress bars for goals", "Table filter for employee list"]
+    },
+    {
+        "image_url": "",
+        "title": "Finance Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Monthly expense vs income chart", "Category-wise expense pie chart"]
+    },
+    {
+        "image_url": "",
+        "title": " Inventory Management Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Stock levels (with warnings for low stock)", "Inventory aging report"]
+    },
+    {
+        "image_url": "",
+        "title": "Project Management Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Drag and drop task management (Kanban board style)", "Deadline warnings"]
+    },
+    {
+        "image_url": "",
+        "title": "Customer Support Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Number of open vs closed tickets", "Customer satisfaction ratings"]
+    },
+    {
+        "image_url": "",
+        "title": "E-commerce Business Dashboard",
+        "Category_Badge": "Business Dashboards",
+        "Tech_Stack_Badges": ["Total orders, total revenue, returning vs new customers", "Graphs by product category"]
+    }
+] 
