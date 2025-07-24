b21cd1f48bf18e03c1ed2e408f11ffd896948e5a
SudeepNPatil <sudeepbsc02@gmail.com>
2025-07-24 15:55:55 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://student-sup.netlify.app/"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
