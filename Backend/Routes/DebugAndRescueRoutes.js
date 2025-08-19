0ec5298314bc887706862e1925cd9e06d007f2a0
SudeepNPatil <sudeepbsc02@gmail.com>
2025-08-19 12:33:20 +0530

    updated

--- Backend/Routes/DebugAndRescueRoutes.js
+++ Backend/Routes/DebugAndRescueRoutes.js
@@ -1,42 +1,43 @@
-import express from 'express'
-import DebugAndRescue from '../models/DebugAndRescue.model.js'
-import multer from "multer";
-
+import express from 'express';
+import DebugAndRescue from '../models/DebugAndRescue.model.js';
+import multer from 'multer';
 
 const router = express.Router();
-
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post('/', upload.single("screenshots"), async (req, res) => {
+router.post('/', upload.single('screenshots'), async (req, res) => {
+  const { name, whatsapp, describe } = req.body;
 
-    const { name, whatsapp, describe } = req.body;
+  try {
+    const newDebugAndRescue = new DebugAndRescue({
+      name,
+      whatsapp,
+      describe,
+      screenshots: {
+        data: req.file.buffer,
+        contentType: req.file.mimetype,
+        size: req.file.size,
+        fileName: req.file.originalname,
+      },
+    });
 
-    try {
+    const saveddata = await newDebugAndRescue.save();
 
-        const newDebugAndRescue = new DebugAndRescue({
-            name,
-            whatsapp,
-            describe,
-            screenshots: {
-                data: req.file.buffer,
-                contentType: req.file.mimetype,
-                size: req.file.size,
-                fileName: req.file.originalname
-            }
-        })
+    const imageBase64 = saveddata.image.toString('base64');
 
-        const saveddata = await newDebugAndRescue.save();
-
-        res.status(201).json({ message: "We Review your request and will connect with you soon..!", data: saveddata })
-
-    }
-    catch (error) {
-        console.error("/DebugAndRescue route error", error.message);
-        res.status(500).json({ message: "server error" });
-    }
-})
-
+    res.status(201).json({
+      message: 'We Review your request and will connect with you soon..!',
+      data: {
+        ...saveddata._doc,
+        image: `data:image/png;base64,${imageBase64}`,
+      },
+    });
+  } catch (error) {
+    console.error('/DebugAndRescue route error', error.message);
+    res.status(500).json({ message: 'server error' });
+  }
+});
 
 export default router;
