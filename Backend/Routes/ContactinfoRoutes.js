4706d544a3f3de87552a047f98b67ce74e04c4e7
SudeepNPatil <sudeepbsc02@gmail.com>
2025-09-30 23:45:35 +0530

    updated

--- Backend/Routes/ContactinfoRoutes.js
+++ Backend/Routes/ContactinfoRoutes.js
@@ -1,35 +1,31 @@
-import express from "express";
-import Contactinfo from "../models/Contactinfo.model.js";
+import express from 'express';
+import Contactinfo from '../models/Contactinfo.model.js';
 
-
-
-const router = express.Router()
-
-
+const router = express.Router();
 
 router.post('/', async (req, res) => {
+  const { name, service, Budget, Email, text } = req.body;
 
-    const { name, service, Budget, Email, text } = req.body;
+  try {
+    const newcontactinfo = new Contactinfo({
+      name,
+      Email,
+      service,
+      Budget,
+      text,
+    });
 
-    try {
+    await newcontactinfo.save();
 
-        const newcontactinfo = new Contactinfo({
-            name,
-            Email,
-            service,
-            Budget,
-            text
-        })
-
-        await newcontactinfo.save();
-
-        res.status(201).json({ message: "We Review your request and will connect with you soon..!" })
-
-    }
-    catch (error) {
-        console.error("/Contactinfo route", error.message);
-        res.status(500).json({ message: "server error" });
-    }
+    res
+      .status(201)
+      .json({
+        message: 'We Review your request and will connect with you soon..!',
+      });
+  } catch (error) {
+    console.error('/Contactinfo route', error.message);
+    res.status(500).json({ message: 'server error' });
+  }
 });
 
 export default router;
